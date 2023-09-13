aeb60139b079c72e9bcc742c304abaec2cf0b8c0
Sanchir01 <emgushovs@mail.ru>
2023-09-13 22:00:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+import Header from './components/Header/Header'
+import SideBar from './components/SideBar/SideBar'
+
+function App() {
+	return (
+		<>
+			<Header />
+			<SideBar />
+		</>
+	)
+}
+
+export default App
